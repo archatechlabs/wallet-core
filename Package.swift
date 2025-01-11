52042a2d5175f8cc14e1185b8eaba0d39a4ddbcc
Archatech Labs <164430145+archatechlabs@users.noreply.github.com>
2025-01-11 13:30:03 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,7 @@ let package = Package(
     name: "WalletCore",
     platforms: [.iOS(.v13)],
     products: [
-        .library(name: "WalletCore", targets: ["WalletCore"]),
-        .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
+        .library(name: "WalletCore", targets: ["WalletCore"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMinor(from: "1.19.0"))
@@ -16,10 +15,6 @@ let package = Package(
             name: "WalletCore",
             url: "https://github.com/trustwallet/wallet-core/releases/download/2.9.5/WalletCore.xcframework.zip",
             checksum: "bb3bccd29e4f03c58bfa1fd746a0f8d6fc4ffc9ec2183beb4facd7b01974a9a2"
-        ),
-        .target(
-            name: "SwiftProtobuf",
-            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")]
         )
     ]
 )
